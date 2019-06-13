27005d479a611865329b8d58b506a339b051cc13
arshad408 <arshadachu408@gmail.com>
2019-06-13 11:43:13 +0530

    Update set2p3.py

--- set2p3.py
+++ set2p3.py
@@ -1,12 +1,12 @@
-v,r=map(int,input().split())
+x,r=map(int,input().split())
 p=list(map(int,input().split()))
-t=[]
+z=[]
 for i in range(0,r):
-    f=[]
-    f=list(map(int,input().split()))
+    x=[]
+    x=list(map(int,input().split()))
     q=f[0]
-    for j in range(min(f)-1,max(f)):
+    for j in range(min(x)-1,max(x)):
         if q>p[j]: q=p[j]
-    t.append(q)
-for i in range(0,len(t)):
-    print(t[i])
+    z.append(q)
+for i in range(0,len(z)):
+    print(z[i])
